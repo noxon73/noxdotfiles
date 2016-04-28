a67be05760c39be25a0dc7d059c52f7e4767912b
Ex4 <ex4@secure-you.de>
2016-04-29 00:24:56 +0200

    generalize path

--- is24/dotfilelinker.sh
+++ is24/dotfilelinker.sh
@@ -1,20 +1,17 @@
 #! /bin/bash
-#set -x
-MYPATH=~/github/noxdotfiles/is24
+set -x
+#MYPATH=~/github/noxdotfiles/is24
 # assure that this is run from actual dir i.e ./dotfilelinker.sh
 if [[ $0 != ./dotfilelinker.sh ]];then
     echo "Please run this script from within the dir to avoid problems. Exit!"
     exit 4
 fi
-if ! [ -d $MYPATH ] ; then
-	echo "expected checkout in $MYPATH"
-fi
 
-cd /
+MYPATH=$(pwd)
 
 for dotfile in $(find $MYPATH -maxdepth 1| grep "/"); do
     file=$(basename $dotfile)
-    if [ -f ~/$file -a ! -L ~/$file ]; then
+    if [ -e ~/$file -a ! -L ~/$file ]; then
 	    mv ~/$file ~/$file.orig-$(date +%F)
     fi
     if ! [ -L ~/$file ]; then
