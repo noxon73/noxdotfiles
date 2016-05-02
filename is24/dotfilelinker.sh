04c6464ea46b7d7c8219206404bb74a20d49e9e6
Eric Grehm <eric.grehm@immobilienscout24.de>
2016-05-02 18:25:35 +0200

    a bit more partial

--- is24/dotfilelinker.sh
+++ is24/dotfilelinker.sh
@@ -10,13 +10,26 @@ MYPATH=$(pwd)
 
 for dotfile in $(find $MYPATH -maxdepth 1| grep "/"); do
     file=$(basename $dotfile)
-    if [ -e ~/$file -a ! -L ~/$file ]; then
-	    mv ~/$file ~/$file.orig-$(date +%F)
-    fi
     if [ x$file == xpartial ] ; then
         # only link single files within instead of whole dir
-        echo "partial not yet implemented"
+        echo "partial not yet implemented. Please link yourself if needed"
         continue
+
+#        ### fix later
+#        cd $file
+#        for DIR in $(find  -type d | sed s'#./#~/#' | grep /); do
+#            mkdir -p $DIR
+#        done
+#        for FILE in $(find  -type d | sed s'#./#~/#' | grep /); do
+#            if [ -e $FILE -a ! -L $FILE ]; then
+#	            mv $FILE $FILE.orig-$(date +%F)
+#            fi
+#        done
+#        continue
+#        #### 
+    fi
+    if [ -e ~/$file -a ! -L ~/$file ]; then
+	    mv ~/$file ~/$file.orig-$(date +%F)
     fi
     if ! [ -L ~/$file ]; then
         if ! [ x$file == xdotfilelinker.sh ]; then
