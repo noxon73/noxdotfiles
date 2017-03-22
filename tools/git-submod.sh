2e484594a57a100fdef04fa039f22375982d616d
hetz <git-hetz@noxlux.de>
2017-03-22 10:20:44 +0100

    submodule refactor ...again :/

---
+++ tools/git-submod.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+git submodule add git://github.com/jmcantrell/vim-virtualenv.git dotfiles/.vim/bundle/vim-virtualenv
+git submodule add git://github.com/jmcantrell/vim-virtualenv.git dotfiles/.vim/bundle/vim-virtualenv
+git submodule add git://github.com/jmcantrell/vim-virtualenv.git dotfiles/.vim/bundle/vim-virtualenv
+git submodule add https://github.com/junegunn/seoul256.vim.git dotfiles/.vim/bundle/seoul256.vim
+git submodule add git://github.com/scrooloose/syntastic.git dotfiles/.vim/bundle/syntastic
+git submodule add git://github.com/freeo/vim-kalisi.git dotfiles/.vim/bundle/vim-kalisi
+git submodule add git://github.com/scrooloose/nerdtree dotfiles/.vim/bundle/nerdtree
+git submodule add git://github.com/majutsushi/tagbar.git dotfiles/.vim/bundle/tagbar
+git submodule add git://github.com/hdima/python-syntax.git dotfiles/.vim/bundle/python-syntax
+git submodule add git://github.com/vim-airline/vim-airline.git dotfiles/.vim/bundle/vim-airline
+git submodule add https://github.com/tpope/vim-fugitive.git dotfiles/.vim/bundle/vim-fugitive
+git submodule add https://github.com/vim-scripts/pep8.git dotfiles/.vim/bundle/pep8
+git submodule add https://github.com/vim-scripts/Gundo.git dotfiles/.vim/bundle/Gundo
+git submodule add  https://github.com/vim-scripts/xoria256.vim.git dotfiles/.vim/bundle/xoria256.vim
+git submodule add https://github.com/cykerway/complete-alias.git dotfiles/complete-alias
